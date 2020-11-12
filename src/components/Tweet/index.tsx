b012865fa71ab3e06a128889fd3b103238b5ad47
Jose Bustillo <josebustillodealba@gmail.com>
2020-11-12 14:38:56 -0500

    feat: set the date from the backend

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,30 +1,29 @@
 import * as React from 'react';
 import './index.scss';
 import { ITweetResume } from '../../util/interfaces';
+import { Link } from 'react-router-dom';
  
 const Tweet = (props: {tweet: ITweetResume}) => {
     return (
         <div className="tweet-container">
             <div className="tweet-one">
-                <p className="username-p">{props.tweet.username}</p>
-                <p>{props.tweet.user}</p>
-                <p>{props.tweet.date}</p>
+                <p className="username-p">{props.tweet.name}</p>
+                <p>@{props.tweet.username}</p>
+                <p>{props.tweet.createdAt.substring(0, props.tweet.createdAt.indexOf("T"))}</p>
             </div>
             <div className="tweet-two">
-                <p>Polarización: {props.tweet.polarizationRate}</p>
-                <p>Político: {props.tweet.politicalRate}</p>
+                {/* <p>Polarización: {props.tweet.polarizationRate}</p>
+                <p>Político: {props.tweet.politicalRate}</p> */}
+                <p>Polarización: 70</p>
+                <p>Político: 30</p>
                 <p>
-                    <a
-                        href={props.tweet.profileLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >Ir al perfil</a>
+                    <Link
+                        to={`/${props.tweet.username}`}
+                    >Perfil</Link>
                     &nbsp;
-                    <a
-                        href={props.tweet.tweetLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >Ver detalles</a>
+                    <Link
+                        to={`/${props.tweet.username}/${props.tweet.tweetId}`}
+                    >Detalles</Link>
                 </p>
             </div>
         </div>
