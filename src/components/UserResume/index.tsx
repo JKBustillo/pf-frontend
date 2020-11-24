5839b42907cfb5a85199949d62114d8979337c60
Jose Bustillo <josebustillodealba@gmail.com>
2020-11-24 14:20:36 -0500

    feat: change the values to regions

--- src/components/UserResume/index.tsx
+++ src/components/UserResume/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { IUserResume } from '../../util/interfaces';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './index.scss';
  
 const UserResume = (props: { user: IUserResume }) => {
     return (
         <div className="user-container">
             <div className="user-left">
-                <p className="profile-name">{props.user.name}</p>
-                <p>@{props.user.username}</p>
+                <p className="profile-name">{props.user._id}</p>
+                {/* <p>@{props.user.username}</p> */}
             </div>
             <div className="user-right">
                 <p>Incidencias: {props.user.counter}</p>
-                <p><Link to={props.user.username}>Ir al perfil</Link></p>
+                {/* <p><Link to={props.user.username}>Ir al perfil</Link></p> */}
             </div>
         </div>
     );
