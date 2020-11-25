f607d262e2d206dc60902532f8d4e35a75854fd8
Jose Bustillo <josebustillodealba@gmail.com>
2020-11-24 20:25:55 -0500

    feat: add link to the region details

--- src/components/UserResume/index.tsx
+++ src/components/UserResume/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IUserResume } from '../../util/interfaces';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './index.scss';
  
 const UserResume = (props: { user: IUserResume }) => {
@@ -8,11 +8,10 @@ const UserResume = (props: { user: IUserResume }) => {
         <div className="user-container">
             <div className="user-left">
                 <p className="profile-name">{props.user._id}</p>
-                {/* <p>@{props.user.username}</p> */}
             </div>
             <div className="user-right">
                 <p>Incidencias: {props.user.counter}</p>
-                {/* <p><Link to={props.user.username}>Ir al perfil</Link></p> */}
+                <p><Link to={`/region/${props.user._id}`}>Ir a la región</Link></p>
             </div>
         </div>
     );
